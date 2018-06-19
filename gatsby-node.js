2352c94486cad16634a5b9bd32bee683dc7e8f7b
Kostas Bariotis <konmpar@gmail.com>
2018-06-19 11:57:56 +0300

    bing bang

---
+++ gatsby-node.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+exports.createPages = async ({ actions, graphql }, options) => {
+  const { createPage } = actions;
+
+  const promises = options.sources.map(async (option) => {
+    const results = await graphql(option.query)
+    if (results.errors) {
+      return Promise.reject(results.errors);
+    }
+
+    Object.keys(results).map(prop => {
+      let feed = results[prop];
+      if (option.serialize && option.serialize instanceof Function) {
+        feed = option.serialize.call(null, results);
+      }
+      createPagination(createPage, feed, option.path);
+    })
+  })
+
+  await Promise.all(promises)
+
+}
+
+/**
+ * Create pagination for posts
+ */
+function createPagination(createPage, edges, pathPrefix) {
+  const pageTemplate = path.resolve(`src/templates/page.js`);
+
+  const pageSize = 5;
+  const pagesSum = Math.ceil(edges.length / pageSize);
+
+  for (let page = 1; page <= pagesSum; page++) {
+    console.log(`${pathPrefix}/${page}`)
+    createPage({
+      path: `${pathPrefix}/${page}`,
+      component: pageTemplate,
+      context: {
+        posts: paginate(edges, pageSize, page).map(({ node }) => node),
+        page,
+        pagesSum,
+        prevPath: page - 1 > 0 ? `${pathPrefix}/${page - 1}` : null,
+        nextPath: page + 1 <= pagesSum ? `${pathPrefix}/${page + 1}` : null,
+      },
+    });
+  }
+}
+
+function paginate(array, page_size, page_number) {
+  return array.slice(0).slice((page_number - 1) * page_size, page_number * page_size);
+}
